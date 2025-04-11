b8f02a2a44a41f80c8545ed496134a3cb43942bc
Enzo <eviniciuskohls@gmail.com>
2025-04-10 21:36:45 -0300

    ObjetoseClasses

---
+++ POO/src/AutoCar/Menu.java
@@ -0,0 +1,24 @@
+package AutoCar;
+
+import java.util.Scanner;
+
+public class Menu {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        Veiculo veiculo = new Veiculo();
+
+        System.out.println("Quantidade de Rodas: ");
+        veiculo.setQuantidadesRodas(sc.nextInt());
+        System.out.println("Chassi: ");
+        veiculo.setChassi(sc.next());
+        System.out.println("Marca: ");
+        veiculo.setMarca(sc.next());
+        System.out.println("Tipo: ");
+        veiculo.setTipo(sc.next());
+
+
+        System.out.println(veiculo.toString());
+    }
+}
--- POO/src/Calculadora.java
+++ POO/src/Calculadora.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Calculadora {
@@ -11,13 +13,8 @@ public class Calculadora {
         num1 = sc.nextDouble();
         System.out.println("Escreva o segundo valor: ");
         num2 = sc.nextDouble();
-        System.out.println("Opção de calculo: ");
-        System.out.println("1 - Adição");
-        System.out.println("2 - Subtração");
-        System.out.println("3 - Multiplicação");
-        System.out.println("4 - Divisão");
-        System.out.println("Escolha: ");
-        tipoCalculo = sc.nextInt();
+
+        tipoCalculo = opcaoCalculo();
 
         if (tipoCalculo == 1) {
             System.out.println("O Resultado é: " + (num1 + num2));
@@ -28,5 +25,17 @@ public class Calculadora {
         } else if (tipoCalculo == 4) {
             System.out.println("O Resultado é: " + (num1 / num2));
         }
+    }
+
+    public static int opcaoCalculo() {
+        System.out.println("Opção de calculo: ");
+        System.out.println("1 - Adição");
+        System.out.println("2 - Subtração");
+        System.out.println("3 - Multiplicação");
+        System.out.println("4 - Divisão");
+        System.out.println("Escolha: ");
+
+        int tipoCalculo = sc.nextInt();
+        return tipoCalculo;
     }
 }
